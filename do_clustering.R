e903394b495be393d9249f78122f22b28d99d07c
Patches <jacob@jacobshufro.com>
2011-11-12 17:27:40 -0500

    eek

--- do_clustering.R
+++ do_clustering.R
@@ -1,6 +1,16 @@
 require(RSQLite);
 require(cluster);
 
+
+#Functions:
+#sql.pam <- function(x, k, table, connect){
+#	#get the size of the database.
+#	print(dbGetQuery(connect, paste("SELECT count(\"row_names\") from ",table,sep=""))[1])
+#	#Pick k random data points, make them mediods, save them back to the db
+#	
+#}
+
+#Main operations:
 ## Load the data from SQL
 drin <- dbDriver("SQLite")
 fin <- "baa.ratios.sqlite"
@@ -21,8 +31,10 @@ con <- dbConnect(mm, dbname = fileout)
 #### This part below can be repeated as many times as cluster sets wanted
 
 ## do the clustering
-k.ratios <- pam(ratios[0:999,], 15, cluster.only=TRUE)
+#k.ratios <- pam(ratios[0:999,], 15, cluster.only=TRUE)
+k.ratios <- sql.pam(ratios, 50, "ba_ratios", conin)
+
 
 ## Write the ratios to the DB
-dbWriteTable(con, "k15", data.frame(k.ratios))
+#dbWriteTable(con, "k15", data.frame(k.ratios))
 
